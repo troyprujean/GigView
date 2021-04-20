4a2c3d5f29c80004836be2335b5ebddcb9eb2170
troy <troy.prujean@radfords.co.nz>
2021-04-20 13:26:17 +1200

    add efcore

---
+++ GigData/GigContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GigData
+{
+    public class GigContext : DbContext
+    {
+        //public DbSet<Blog> Blogs { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //    => options.U(@"Data Source=C:\blogging.db");
+    }
+}
